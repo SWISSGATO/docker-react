478935f3e5a74165c58eb8eb8ec24cc4bd93d132
Marcel Mosimann INI-EOS-EPS <marcel.mosimann1@swisscom.com>
2023-10-29 11:00:26 +0100

    V3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React for sure 2023-10-29 11:00
         </a>
       </header>
     </div>
